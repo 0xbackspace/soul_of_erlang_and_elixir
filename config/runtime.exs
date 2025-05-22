49c38660af4bc6ba635ead7b301d7309f9eebfee
Himanshu Pal <hpal@roambee.com>
2025-05-22 04:05:31 +0000

    chore: update config for deployment
    Signed-off-by: Himanshu Pal <hpal@roambee.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -23,7 +23,23 @@ end
 if config_env() == :prod do
   config :my_system, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
-  config :my_system, MySystemWeb.Endpoint, url: [host: "localhost", scheme: "http"]
+  host = System.get_env("RENDER_EXTERNAL_HOSTNAME") || System.get_env("PHX_HOST") || "localhost"
+  port = String.to_integer(System.get_env("PORT") || "4000")
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  config :my_system, MySystemWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
+    ],
+    secret_key_base: secret_key_base
 
   # ## SSL Support
   #
